b438f9f743ddaa574e26c761e32c2c10c6d95248
Navas <gabrielnavas@email.com>
2021-02-22 15:41:41 -0300

    add chat screen created

--- src/screens/AddChatScreen.js
+++ src/screens/AddChatScreen.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useLayoutEffect, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import {Input, Button} from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
-const AddChatScreen = () => {
+const AddChatScreen = ({navigation}) => {
+
+  const [input, setInput] = useState('')
+
+  const createChat = useCallback(() => {
+  }, [])
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Add a new chat',
+      headerBackTitle: 'chats' //only IOS
+    })
+  }, [navigation])
+
   return (
-    <View>
-      <Text>This is Add Chat Screen</Text>
+    <View style={styles.container}>
+      <Input 
+        placeholder='Enter a chat name'
+        value={input}
+        onChangeText={text => setInput(text)}
+        leftIcon={
+          <Icon name='wechat' type='antdesign' size={24} color='black' />
+        }
+        onSubmitEditing={createChat}
+      />
+      <Button 
+        onPress={createChat}
+        title='Create new Chat'
+      />
     </View>
   )
 }
 
 export default AddChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {}
+})

60f46be603a5ad020292c144547e9a47326534fa
Navas <gabrielnavas@email.com>
2021-02-22 15:29:13 -0300

    finish home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { SafeAreaView, StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons'
 
 import * as userAsyncStorage from '../services/user/local-storage-user'
 
@@ -18,25 +19,51 @@ const HomeScreen = ({ navigation }) => {
   }, [])
 
   useLayoutEffect(() => {
+    const afterGetUser = ({ user }) => {
+      setUser(user)
+      navigation.setOptions({
+        title: "Signal",
+        headerLeft: null,
+        headerStyle: { backgroundColor: '#fff' },
+        headerTitleStyle: { color: 'black' },
+        headerTintColor: 'black',
+        headerLeft: () => (
+          <View style={{ marginLeft: 20 }}>
+            <TouchableOpacity activeOpacity={0.5} >
+              <Avatar rounded source={{ uri: user.imageURL }} />
+            </TouchableOpacity>
+          </View>
+        ),
+        headerRight: () => (
+          <View style={{
+            flexDirection: 'row', 
+            justifyContent: 'space-between',
+            width: 80,
+            marginRight: 20
+          }}>
+            <TouchableOpacity activeOpacity={0.5}>
+              <AntDesign
+                name='camerao'
+                size={24}
+                color='black'
+              />
+            </TouchableOpacity>
+            <TouchableOpacity 
+              onPress={() => navigation.navigate('AddChat')}  
+              activeOpacity={0.5}>
+              <SimpleLineIcons
+                name='pencil'
+                size={24}
+                color='black'
+              />
+            </TouchableOpacity>
+          </View>
+        )
+      })
+    }
     userAsyncStorage
       .getUserLocalStorage()
-      .then(({ user }) => {
-        setUser(user)
-        navigation.setOptions({
-          title: "Signal",
-          headerLeft: null,
-          headerStyle: { backgroundColor: '#fff' },
-          headerTitleStyle: { color: 'black' },
-          headerTintColor: 'black',
-          headerLeft: () => (
-            <View style={{ marginLeft: 20 }}>
-              <TouchableOpacity>
-                <Avatar rounded source={{ uri: user.imageURL }} />
-              </TouchableOpacity>
-            </View>
-          )
-        })
-      })
+      .then(afterGetUser)
   }, [])
 
   return (
